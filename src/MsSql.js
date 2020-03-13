67e82611fd25d033bd7d6637f6ac7090d9ee9034
David Menger <david.menger@me.com>
2020-03-13 23:08:26 +0100

    ability to reuse pool

--- src/MsSql.js
+++ src/MsSql.js
@@ -19,6 +19,10 @@ class MsSql {
      * @param {number} config.port
      * @param {string} config.database
      * @param {object} config.options
+     * @param {object} [config.mssql]
+     * @param {Function} [config.pool]
+     * @param {string} [config.wingbotMigrationsTable]
+     * @param {object} config.options
      * @param {boolean} config.options.encrypt
      * @param {string|null} migrationsDir
      * @param {console} log
@@ -26,36 +30,43 @@ class MsSql {
     constructor (config, migrationsDir = null, log = console) {
         this._config = config;
         this._migrationsDir = migrationsDir || path.resolve(__dirname, '..', 'migrations');
+        this._migrationsTable = config.wingbotMigrationsTable || undefined;
         this._log = log;
+
+        this._mssql = config.mssql || mssql;
 
         this._pool = null;
     }
 
     async _createConnectionPoll () {
         try {
-            let config;
-            if (this._config.password instanceof Promise) {
-                const password = await this._config.password;
-                config = {
-                    ...this._config,
-                    password
-                };
-            } else {
-                config = this._config;
+            let { pool = null } = this._config;
+
+            if (!pool) {
+                let config;
+                if (this._config.password instanceof Promise) {
+                    const password = await this._config.password;
+                    config = {
+                        ...this._config,
+                        password
+                    };
+                } else {
+                    config = this._config;
+                }
+
+                // @ts-ignore
+                const cp = new this._mssql.ConnectionPool(config);
+
+                cp.on('error', (err) => {
+                    this._log.error('MSSQL ERROR', err);
+                });
+
+                pool = cp.connect();
             }
-
-            // @ts-ignore
-            const cp = new mssql.ConnectionPool(config);
-
-            cp.on('error', (err) => {
-                this._log.error('MSSQL ERROR', err);
-            });
-
-            const pool = cp.connect();
 
             const connection = await pool;
 
-            const migrate = new Migrate(pool, this._migrationsDir);
+            const migrate = new Migrate(pool, this._migrationsDir, this._migrationsTable);
 
             await migrate.migrate();
 
